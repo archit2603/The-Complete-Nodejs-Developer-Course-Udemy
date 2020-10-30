dc089628006f4834e05daff792b6fba7926353f8
archit2603 <archit_1901mm09@iitp.ac.in>
2020-10-30 11:41:16 +0530

    added remaining files

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -1,8 +1,9 @@
 const path = require('path')
-const express = requre('express')
-const hbs = requre('hbs')
+const express = require('express')
+const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -22,7 +23,64 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index',{
-        title: 'weather',
+        title: 'Weather',
         name: 'Archit Anand'
     })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Archit Anand'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Archit Anand'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error){
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send(error)
+            }
+            res.send({
+                location: location, 
+                forecast: forecastData,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        message: 'Help article not found',
+        title: 404,
+        name: 'Archit Anand'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error',{
+        message: 'Page not found',
+        title: 404,
+        name: 'Archit Anand'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
