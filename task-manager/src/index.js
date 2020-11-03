72b44e75bcaa6e04662d9c708a8f07ef9325f9be
archit2603 <archit_1901mm09@iitp.ac.in>
2020-11-03 16:52:31 +0530

    Added update operations and .gitignore

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -76,6 +76,50 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+        return res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
+
+        if(!task){
+            return res.status(404).send()
+        }
+        return res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port '+port)
 })
