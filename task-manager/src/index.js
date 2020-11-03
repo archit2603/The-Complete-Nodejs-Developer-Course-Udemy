4e5099f355d9cfbda46ff8931bd45cb796954f00
archit2603 <archit_1901mm09@iitp.ac.in>
2020-11-03 23:44:02 +0530

    added start and dev scripts to package.json

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,8 +2,6 @@ const express = require('express')
 require('./db/mongoose')
 const userRoute = require('./routes/user')
 const taskRoute = require('./routes/task')
-const bcrypt = require('bcryptjs')
-
 const app =  express()
 const port = process.env.PORT || 3000
 
