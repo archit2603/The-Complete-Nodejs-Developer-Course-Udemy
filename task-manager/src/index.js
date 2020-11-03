c3c3a0e8048de0a6321d1486be814536c652bdeb
archit2603 <archit_1901mm09@iitp.ac.in>
2020-11-03 23:41:58 +0530

    added password hashing and login method

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,124 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRoute = require('./routes/user')
+const taskRoute = require('./routes/task')
+const bcrypt = require('bcryptjs')
 
 const app =  express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.post('/task', async (req, res) => {
-    const task = new Task(req.body)
-
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send()
-    }
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        return res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    
-    try {
-        const task = await Task.findById(_id)
-        if(!task){
-            return res.status(400).send()
-        }
-        return res.send(task)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation){
-        return res.status(400).send({error: 'Invalid updates!'})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
-
-        if(!user){
-            return res.status(404).send()
-        }
-        return res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.patch('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation){
-        return res.status(400).send({error: 'Invalid updates!'})
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
-
-        if(!task){
-            return res.status(404).send()
-        }
-        return res.send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+app.use(userRoute)
+app.use(taskRoute)
 
 app.listen(port, () => {
     console.log('Server is up on port '+port)
